c46570709b502608d271ee3382875b2001407e08
Mikhail Douzhyk <mdouzhyk@gmail.com>
2025-02-11 20:40:37 +0300

    added list to is

--- test1/is.py
+++ test1/is.py
@@ -1,4 +1,34 @@
-d = 100087
-e = 100087
-#update to main
-print(d == e)
+# a = 'test'
+# b = 'best'
+
+# text = f'Message is {a}, because it is the {b.upper()}'
+
+# # print(text)
+
+# my_list = [1, 3, 5.6, None, 'test', ['matreshka', 228], True]
+
+# # Индекс с конца (первый с конца) всегда равен -1 Всегда последний элемент списка
+# # print(my_list)
+# print(f'Last element of the list - {my_list[-1]}')
+
+# my_list[-2] = 'groove_st'
+# my_list[2] = my_list[1]
+# empty_list = []
+# empty_list_function = list()
+
+# # print(my_list)
+
+# # print(empty_list_function)
+
+# empty_list.append('hello friends')
+
+# print(len(empty_list))
+# print(my_list.index('test'))
+
+# my_list.pop(3)
+
+# print(my_list)
+
+new_list = [1, 3, 5.6, None, 'test', ['matreshka', 228], True]
+new_list.pop(3)
+print(new_list)
